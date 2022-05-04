b20b7787fdcaff4990112cbff466a74bea35c98b
Arthur Carneiro Lobo <arthurlbo16@gmail.com>
2022-05-04 14:46:42 -0300

    Inclusão do widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,5 @@
-interface ButtonProps{
-    text?: string;
+import { Widget } from './Components/Widget';
+
+export function App() {
+    return <Widget />;
 }
-
-function Button(props: ButtonProps){
-    return <button className=" bg-[#8257e6]  px-3 h-10 rounded text-violet-100 hover:bg-violet-700 transition-colors" >{props.text ?? 'default'}</button>
-}
-
-function App() {
-
-    return <div className="flex gap-2">
-
-        <Button  text = "enviar" />
-        <Button  text = "ok" />
-        <Button />
-    </div>
-}
-
-export default App
