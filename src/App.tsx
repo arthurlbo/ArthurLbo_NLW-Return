27a033730f260fa76318a999234abab863bef9ec
Arthur Carneiro Lobo <arthurlbo16@gmail.com>
2022-05-04 13:19:35 -0300

    att1

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ interface ButtonProps{
 }
 
 function Button(props: ButtonProps){
-    return <button>{props.text ?? 'default'}</button>
+    return <button className=" bg-[#8257e6]  px-3 h-10 rounded text-violet-100 hover:bg-violet-700 transition-colors" >{props.text ?? 'default'}</button>
 }
 
 function App() {
 
-    return <div>
+    return <div className="flex gap-2">
 
         <Button  text = "enviar" />
         <Button  text = "ok" />
