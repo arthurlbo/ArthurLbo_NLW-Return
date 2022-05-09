f8a349cec80f31747869d2b435af8fa47679a13e
Arthur Carneiro Lobo <arthurlbo16@gmail.com>
2022-05-09 10:19:08 -0300

    AddMobile

---
+++ Mobile/Server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,23 @@
+import nodemailer from'nodemailer'
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const  transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "658e1ee3d913b8",
+    pass: "6bedcb5bfa7192"
+  }
+});
+
+export class NodemailerMailAdapter implements MailAdapter{
+    async sendMail({subject, body}: SendMailData) {
+        const feedback = await transport.sendMail({
+        from: 'Equipe Feedget <equipe@feedget.com>',
+        to: 'Arthur C Lobo <aclskt16@gmail.com',
+        subject,
+        html: body,
+    })
+
+    }
+}
