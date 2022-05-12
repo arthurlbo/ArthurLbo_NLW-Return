6b72be1dd0da047341b604cc6c9e08ca428279cd
Arthur Carneiro Lobo <arthurlbo16@gmail.com>
2022-05-12 14:20:22 -0300

    AddLayout

--- Web/src/App.tsx
+++ Web/src/App.tsx
@@ -10,6 +10,7 @@ export function App() {
                 window.matchMedia('(prefers-color-scheme: dark)').matches)
         ) {
             document.documentElement.classList.add('dark');
+            
         } else {
             document.documentElement.classList.remove('dark');
         }
